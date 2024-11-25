9e7c47a7ea937db9350ee44c9ebaa23eb3440b23
Ranko Radonic <ranko.radonic@gmail.com>
2024-11-25 21:05:36 +0000

    WIP

--- src/network.ts
+++ src/network.ts
@@ -1,4 +1,4 @@
-import { random, Matrix, matrix, dotMultiply, add } from "mathjs";
+import { random, Matrix, matrix, row, dot, add, MathArray } from "mathjs";
 
 import { sigmoid } from "./sigmoid.js";
 
@@ -16,14 +16,46 @@ export class Network {
     this.biases = [];
 
     for (let i = 1; i < this.sizes.length; i++) {
-      this.biases.push(matrix(random([this.sizes[i], 1])));
+      // this.biases.push(matrix(random([this.sizes[i], 1])));
+
+      const biases = new Array(this.sizes[i]);
+      biases.fill(3);
+      this.biases.push(matrix(biases));
+
       this.weights.push(matrix(random([this.sizes[i], this.sizes[i - 1]])));
     }
   }
 
-  feedforward(a: Matrix) {
-    for (let i = 1; i < this.sizes.length; i++) {
-      a = sigmoid(add(dotMultiply(this.weights[i], a), this.biases[i]));
+  feedForward(a: Matrix) {
+    for (let i = 0; i < this.sizes.length; i++) {
+      const weights = this.weights[i];
+      const biases = this.biases[i];
+
+      const dotProducts = [];
+
+      for (let j = 0; j < this.sizes[i + 1]; j++) {
+        console.log(j);
+        const r = row(weights, j);
+
+        console.log("###");
+        console.log(r.toArray(), a.toArray());
+        dotProducts.push(dot(r.toArray()[0] as MathArray, a.toArray()));
+      }
+
+      // const multiplied = row(weights, j)
+      // return dot(row, a);
+
+      console.log("---");
+      console.log(weights);
+      console.log(a);
+      console.log(dotProducts);
+
+      process.exit();
+
+      // //dot(weights, a);
+      // const added = add(multiplied, biases);
+
+      // a = sigmoid(added);
     }
   }
 
