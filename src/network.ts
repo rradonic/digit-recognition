28a8d957f0c89ce5289eea5a63399e3209a6c14b
Ranko Radonic <ranko.radonic@gmail.com>
2024-11-25 21:05:36 +0000

    Implement a basic feed forward function

--- src/network.ts
+++ src/network.ts
@@ -1,4 +1,4 @@
-import { random, Matrix, matrix, dotMultiply, add } from "mathjs";
+import { random, Matrix, matrix, row, dot, add, MathArray } from "mathjs";
 
 import { sigmoid } from "./sigmoid.js";
 
@@ -16,20 +16,35 @@ export class Network {
     this.biases = [];
 
     for (let i = 1; i < this.sizes.length; i++) {
+      // const biases = new Array(this.sizes[i]);
+      // biases.fill(3);
+      // this.biases.push(matrix(biases));
+
       this.biases.push(matrix(random([this.sizes[i], 1])));
       this.weights.push(matrix(random([this.sizes[i], this.sizes[i - 1]])));
     }
   }
 
-  feedforward(a: Matrix) {
-    for (let i = 1; i < this.sizes.length; i++) {
-      a = sigmoid(add(dotMultiply(this.weights[i], a), this.biases[i]));
-    }
-  }
+  feedForward(a: Matrix) {
+    for (let i = 0; i < this.sizes.length - 1; i++) {
+      const weights = this.weights[i];
+      const biases = this.biases[i];
 
-  // def feedforward(self, a):
-  //       """Return the output of the network if "a" is input."""
-  //       for b, w in zip(self.biases, self.weights):
-  //           a = sigmoid(np.dot(w, a)+b)
-  //       return a
+      const dotProducts = [];
+
+      for (let j = 0; j < this.sizes[i + 1]; j++) {
+        const r = row(weights, j);
+
+        dotProducts.push(dot(r.toArray()[0] as MathArray, a.toArray()));
+      }
+
+      console.log("---");
+      console.log(dotProducts);
+      console.log(add(dotProducts, biases));
+
+      a = sigmoid(matrix(dotProducts));
+    }
+
+    return a;
+  }
 }
