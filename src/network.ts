cf0847537773437aae599fa6e5c241e8782523ec
Ranko Radonic <ranko.radonic@gmail.com>
2024-12-01 14:48:23 +0000

    Add a constructor that sets weights and biases

--- src/network.ts
+++ src/network.ts
@@ -5,21 +5,27 @@ import { sigmoid } from "./sigmoid.js";
 export class Network {
   numLayers: number;
   sizes: number[];
-  biases: Matrix[];
   weights: Matrix[];
+  biases: Matrix[];
 
-  constructor(sizes: number[]) {
+  constructor(sizes: number[]);
+  constructor(sizes: number[], weights: Matrix[], biases: Matrix[]);
+
+  constructor(sizes: number[], weights?: Matrix[], biases?: Matrix[]) {
     this.numLayers = sizes.length;
     this.sizes = sizes;
+
+    if (weights && biases) {
+      this.weights = weights;
+      this.biases = biases;
+
+      return;
+    }
 
     this.weights = [];
     this.biases = [];
 
     for (let i = 1; i < this.sizes.length; i++) {
-      // const biases = new Array(this.sizes[i]);
-      // biases.fill(3);
-      // this.biases.push(matrix(biases));
-
       this.biases.push(matrix(random([this.sizes[i], 1])));
       this.weights.push(matrix(random([this.sizes[i], this.sizes[i - 1]])));
     }
@@ -34,15 +40,11 @@ export class Network {
 
       for (let j = 0; j < this.sizes[i + 1]; j++) {
         const r = row(weights, j);
-
         dotProducts.push(dot(r.toArray()[0] as MathArray, a.toArray()));
       }
 
-      console.log("---");
-      console.log(dotProducts);
-      console.log(add(dotProducts, biases));
-
-      a = sigmoid(matrix(dotProducts));
+      a = sigmoid(add(dotProducts, biases) as Matrix);
+      console.log(a);
     }
 
     return a;
