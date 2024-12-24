2042bac8d0a609ec92a3a05fd9965aa9ea6e88bd
Ranko Radonic <ranko.radonic@gmail.com>
2024-12-24 00:38:09 +0000

    Rename constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 export const TRAINING_IMAGE_FILE = "./data/train-images-idx3-ubyte";
-export const TESTING_IMAGE_FILE = "./data/t10k-images-idx3-ubyte";
+export const TEST_IMAGE_FILE = "./data/t10k-images-idx3-ubyte";
 
 export const TRAINING_LABEL_FILE = "./data/train-labels-idx1-ubyte";
-export const TESTING_LABEL_FILE = "./data/t10k-labels-idx1-ubyte";
+export const TEST_LABEL_FILE = "./data/t10k-labels-idx1-ubyte";
 
 export const PPM_DIRECTORY = "./data/ppm";
