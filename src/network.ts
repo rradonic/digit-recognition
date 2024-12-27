d7a14d8c3d9c2d57271cdee22d70578fb9a51d9a
Ranko Radonic <ranko.radonic@gmail.com>
2024-12-27 20:29:24 +0000

    Implement gradient descent method skeleton

--- src/network.ts
+++ src/network.ts
@@ -1,9 +1,9 @@
 import * as tf from "@tensorflow/tfjs";
 
-import { sigmoid } from "./sigmoid.js";
+import { sigmoid, sigmoidPrime } from "./sigmoid.js";
+import type { TrainingPair, TestPair } from "./types.js";
 
 export class Network {
-  numLayers: number;
   sizes: number[];
   weights: tf.Tensor[];
   biases: tf.Tensor[];
@@ -12,7 +12,6 @@ export class Network {
   constructor(sizes: number[], weights: tf.Tensor[], biases: tf.Tensor[]);
 
   constructor(sizes: number[], weights?: tf.Tensor[], biases?: tf.Tensor[]) {
-    this.numLayers = sizes.length;
     this.sizes = sizes;
 
     if (weights && biases) {
@@ -27,7 +26,7 @@ export class Network {
 
     for (let i = 1; i < this.sizes.length; i++) {
       this.biases.push(tf.randomNormal([this.sizes[i], 1]));
-      this.weights.push(tf.randomNormal([this.sizes[i + 1], this.sizes[i]]));
+      this.weights.push(tf.randomNormal([this.sizes[i], this.sizes[i - 1]]));
     }
   }
 
@@ -39,5 +38,33 @@ export class Network {
     }
 
     return layer;
+  }
+
+  stochasticGradientDescent(
+    trainingData: TrainingPair[],
+    epochs: number,
+    miniBatchSize: number,
+    eta: number,
+    testData?: TestPair[]
+  ) {
+    for (let i = 0; i < epochs; i++) {
+      tf.util.shuffle(trainingData);
+
+      for (let j = 0; j < trainingData.length; j += miniBatchSize) {
+        const miniBatch = trainingData.slice(j, j + miniBatchSize);
+
+        console.log(`Mini batch starting at ${j}`);
+        // TODO
+        // this.processMiniBatch(miniBatch, eta);
+      }
+
+      // TODO
+      // if (testData) {
+      //   const evaluation = this.evaluate(testData);
+      //   console.log(`Epoch ${i}: ${evaluation} / ${testData.length}`);
+      // } else {
+      //   console.log(`Epoch ${i} complete`);
+      // }
+    }
   }
 }
